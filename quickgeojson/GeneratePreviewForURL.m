ee82556dbc893de8591a69d91125853510cfa37f
Ian <ian@ianrees.net>
2014-06-09 10:21:54 -0700

    Refactoring...

--- quickgeojson/GeneratePreviewForURL.m
+++ quickgeojson/GeneratePreviewForURL.m
@@ -8,12 +8,19 @@
  
 */
 
+static void attach(CFBundleRef* bundle, NSMutableDictionary* attachments, NSString* type, NSString* fileName)
+{
+    CFURLRef attachURL = CFBundleCopyResourceURL(*bundle, (__bridge CFStringRef)(fileName), NULL, NULL);
+    NSData *data = [NSData dataWithContentsOfURL:(__bridge NSURL *)attachURL];
+    NSDictionary* attachment = [NSMutableDictionary dictionaryWithObjectsAndKeys:type, (NSString *)kQLPreviewPropertyMIMETypeKey, data, (NSString *)kQLPreviewPropertyAttachmentDataKey, nil];
+    [attachments setObject:attachment forKey:fileName];
+}
+
 OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview, CFURLRef url, CFStringRef contentTypeUTI, CFDictionaryRef options)
 {
     
-    NSMutableDictionary *props;
     NSMutableString *html;
-
+    
     @autoreleasepool {
 
         // Load the data
@@ -22,28 +29,33 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
         if (!data) return noErr;
         // Convert to NSString
         NSString *geojson = [NSString stringWithUTF8String:[data bytes]];
-        // Test data...:
-        // NSString *geojson = @"{\"type\": \"Feature\",\"properties\": {\"name\": \"Coors Field\",\"amenity\": \"Baseball Stadium\",\"popupContent\": \"A Stadium\"},\"geometry\": {\"type\": \"Point\",\"coordinates\": [-104.99404, 39.75621]}}";
-        
-        props = [[NSMutableDictionary alloc] init];
-        [props setObject:@"UTF-8" forKey:(NSString *)kQLPreviewPropertyTextEncodingNameKey];
-        [props setObject:@"text/html" forKey:(NSString *)kQLPreviewPropertyMIMETypeKey];
-        
-        
-        // Get the template.html file.
+
+        // Bundle reference.
         CFBundleRef bundle = QLPreviewRequestGetGeneratorBundle(preview);
+
+        // Get the template.html file.
         CFURLRef templateURL = CFBundleCopyResourceURL(bundle, (CFStringRef)@"template", (CFStringRef)@"html", NULL);
         if (!templateURL) return noErr;
         CFStringRef templatePath = CFURLCopyFileSystemPath(templateURL, kCFURLPOSIXPathStyle);
         if (!templatePath) return noErr;
-        
         NSError *error;
         NSString *document = [[NSString alloc]
                               initWithContentsOfFile:(__bridge NSString*)templatePath
                               encoding:NSUTF8StringEncoding
                               error:&error];
-        
 
+        // Properties
+        NSMutableDictionary* properties = [NSMutableDictionary dictionary];
+        [properties setObject:@"UTF-8" forKey:(NSString*)kQLPreviewPropertyTextEncodingNameKey];
+        [properties setObject:@"text/html" forKey:(NSString*)kQLPreviewPropertyMIMETypeKey];
+        
+        // Add the attachments.
+        NSMutableDictionary* attachments = [NSMutableDictionary dictionary];
+        attach(&bundle, attachments, @"image/png", @"test.png");
+        attach(&bundle, attachments, @"text/css", @"leaflet.css");
+        attach(&bundle, attachments, @"text/javascript", @"test2.js");
+        [properties setObject:attachments forKey:(NSString*)kQLPreviewPropertyAttachmentsKey];
+        
         // Create the HTML Document...
         html = [[NSMutableString alloc] init];
         // Replace the template {{GEOJSON}} with the GeoJSON data.
@@ -52,11 +64,9 @@ OSStatus GeneratePreviewForURL(void *thisInterface, QLPreviewRequestRef preview,
         QLPreviewRequestSetDataRepresentation(preview,
                                               (__bridge CFDataRef)[html dataUsingEncoding:NSUTF8StringEncoding],
                                               kUTTypeHTML,
-                                              (__bridge CFDictionaryRef)props);
+                                              (__bridge CFDictionaryRef)properties);
         
         return noErr;
-
-        
     }
 }
 
